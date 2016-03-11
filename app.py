d03f12b3d675f1f9620c7575a6926472f7f56c86
admin <admin@admins-Mac-Pro-339.local>
2016-03-11 12:29:03 -0700

    Added a csv file and made a real life data table

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+import csv
 from flask import Flask
 # this is telling it to go to folder flask and import tool Flask
 from flask import render_template
 app = Flask(__name__)
 
+def fetch_csv(csv_path):
+	csv_file = open(csv_path, "rb")
+	csv_obj = csv.DictReader(csv_file)
+		# returns a dictionary (headers)
+	csv_list = list(csv_obj)
+	return csv_list
+
+
 @app.route("/")
 def index():
 	template = "index.html"
-	return render_template(template)
+	object_list = fetch_csv("./static/la-riots-deaths.csv")
+		#the list of data returned by the csv
+	return render_template(template, object_list=object_list)
 
 if __name__ == "__main__":
     # Fire up the Flask test server
