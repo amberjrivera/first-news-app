f1359bbf0d0fe00edfca5c4f21a33db4e9fd04b3
admin <admin@admins-Mac-Pro-339.local>
2016-03-12 10:06:43 -0700

    made a detail page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask
+from flask import abort
 # this is telling it to go to folder flask and import tool Flask
 from flask import render_template
 app = Flask(__name__)
@@ -18,6 +19,15 @@ def index():
 	object_list = fetch_csv("./static/la-riots-deaths.csv")
 		#the list of data returned by the csv
 	return render_template(template, object_list=object_list)
+
+@app.route("/<row_id>/")
+def detail(row_id):
+	template = 'detail.html'
+	object_list = fetch_csv("./static/la-riots-deaths.csv")
+	for row in object_list: 
+		if row['id'] == row_id:
+			return render_template(template, object=row)
+	abort(404)
 
 if __name__ == "__main__":
     # Fire up the Flask test server
